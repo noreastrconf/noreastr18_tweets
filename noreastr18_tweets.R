000a3ca022b394c477d45c01f4adb2719e454cc4
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2018-10-04 20:48:14 -0400

    first grab of noreastr tweets

--- noreastr18_tweets.R
+++ noreastr18_tweets.R
@@ -2,15 +2,15 @@
 library(tweetstorm)
 library(rtweet)
 library(dplyr)
-create_token("noreastr18", 
-             Sys.getenv("noreastr18_key"),
-             Sys.getenv("noreastr18_secret"))
+# Need to create a new app and update...
+create_token("usiale2018", 
+             Sys.getenv("usiale2018_key"),
+             Sys.getenv("usiale2018_secret"))
 if(file.exists("noreastr18.rda")){
   load("noreastr18.rda")
   noreastr18<-tweetstorm:::update_search(noreastr18,"#noreastr18",n=1000)
   save(noreastr18,file="noreastr18.rda")
-}
-else{
+} else {
   noreastr18<-rtweet::search_tweets("#noreastr18",n = 500)
   save(noreastr18, file = "noreastr18.rda")
 }
