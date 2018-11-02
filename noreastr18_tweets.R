7e31f73f347d1fc87f37c9e050f3996d0dddbf7f
Jeff Hollister <jeff.w.hollister@gmail.com>
2018-11-02 08:45:36 -0400

    cleaned up tweets

--- noreastr18_tweets.R
+++ noreastr18_tweets.R
@@ -3,9 +3,9 @@ library(tweetstorm)
 library(rtweet)
 library(dplyr)
 # Need to create a new app and update...
-create_token("usiale2018", 
-             Sys.getenv("usiale2018_key"),
-             Sys.getenv("usiale2018_secret"))
+create_token("noreastr2018", 
+             Sys.getenv("noreastr2018_key"),
+             Sys.getenv("noreastr2018_secret"))
 if(file.exists("noreastr18.rda")){
   load("noreastr18.rda")
   noreastr18<-tweetstorm:::update_search(noreastr18,"#noreastr18",n=1000)
@@ -14,7 +14,5 @@ if(file.exists("noreastr18.rda")){
   noreastr18<-rtweet::search_tweets("#noreastr18",n = 500)
   save(noreastr18, file = "noreastr18.rda")
 }
-
-
 
 
--- setup_app.R
+++
@@ -1,7 +0,0 @@
-cat("noreastr18_key=",
-    file=file.path(normalizePath("~/"), ".Renviron"),
-    append=TRUE)
-
-cat("noreastr18_secret=",
-    file=file.path(normalizePath("~/"), ".Renviron"),
-    append=TRUE)
